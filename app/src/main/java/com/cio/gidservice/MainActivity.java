6e867cabbedf6cabcc2f12dc823870301a553564
yuriy070920 <surzhikov.yura@yandex.ua>
2020-03-12 12:30:04 +0200

    Some changes

--- app/src/main/java/com/cio/gidservice/MainActivity.java
+++ app/src/main/java/com/cio/gidservice/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cio.gidservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.first_registration_activity);
     }
 }
