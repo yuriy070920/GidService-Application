249b551ad878f9b9e8ff7ab2016ecc1cbd80da76
yuriy070920 <surzhikov.yura@yandex.ua>
2020-03-12 13:12:39 +0200

    Added Colors

--- app/src/main/java/com/cio/gidservice/MainActivity.java
+++ app/src/main/java/com/cio/gidservice/MainActivity.java
@@ -3,6 +3,7 @@ package com.cio.gidservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
