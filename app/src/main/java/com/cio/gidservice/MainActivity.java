35ac72133dda0b3cc9015e8c4c46b4cf56927235
yuriy070920 <surzhikov.yura@yandex.ua>
2020-03-13 20:23:46 +0200

    Added Enter name activity

--- app/src/main/java/com/cio/gidservice/MainActivity.java
+++ app/src/main/java/com/cio/gidservice/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.verification_page);
+        setContentView(R.layout.first_registration_page);
     }
 }
