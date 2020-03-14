f74f2eff3eb39caf18bd84aa4c518e6126dcaca8
yuriy070920 <surzhikov.yura@yandex.ua>
2020-03-14 20:56:08 +0200

    Some changes in MainActivity

--- app/src/main/java/com/cio/gidservice/MainActivity.java
+++ app/src/main/java/com/cio/gidservice/MainActivity.java
@@ -1,13 +1,27 @@
 package com.cio.gidservice;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_registration_page);
+
+        findViewById(R.id.firstRegPageNext).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.verification_page);
+            }
+        });
     }
 }
